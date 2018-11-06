d441ad6e8082f64ab43139ecdd3e2b7a1ebee548
hmk12 <44100053+hmk12@users.noreply.github.com>
2018-11-06 09:21:03 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <title>PHP Test</title>
  </head>
  <body>
+ <?php echo '<p>Thank you for visiting Harvey website.</p>'; ?> 
  <?php echo '<p>This is a Blue Green Deployment Strategy</p>'; ?> 
  </body>
 </html>
