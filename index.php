0b16b2c4e50a39317d96f68f02c15716e60f79d8
hmk12 <44100053+hmk12@users.noreply.github.com>
2018-11-09 13:22:14 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <title>PHP Test</title>
  </head>
  <body>
+  <?php echo '<p>First Changes using Pipeline</p>'; ?> 
  <?php echo '<p>Thank you for visiting Harvey website.</p>'; ?> 
  <?php echo '<p>This is a Blue Green Deployment Strategy</p>'; ?> 
  </body>
